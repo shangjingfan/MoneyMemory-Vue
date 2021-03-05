1ed0f2506a2c9641776415e50898c3e3ee71564c
shangjingfan <876625909@qq.com>
2021-03-05 23:17:38 +0800

    正在添加css样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Money.vue
+
+    </div>
     <Nav/>
   </div>
 </template>
@@ -8,9 +11,18 @@
 <script lang="ts">
   export default {
     name: 'Money',
-  }
+  };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
+  .nav-wrapper {
+    border: 1px solid blue;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content{
+    flex-grow: 1;
+  }
 </style>
