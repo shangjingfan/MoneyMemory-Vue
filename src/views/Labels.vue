365f5a8c94f4b5c7c89106e780fa25d7bb08d808
shangjingfan <876625909@qq.com>
2021-03-06 15:33:16 +0800

    添加Nav样式

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -12,14 +12,12 @@
 
 <style scoped lang="scss">
   .nav-wrapper {
-    border: 1px solid blue;
     display: flex;
     flex-direction: column;
     height: 100vh;
   }
 
   .content {
-    border: 1px solid green;
     flex-grow: 1;
     overflow: auto;
   }
