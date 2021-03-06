f6205b55aa42b3cfba3583e8cff099a3a6582f46
shangjingfan <876625909@qq.com>
2021-03-06 15:10:26 +0800

    创建Icon组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,30 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <Icon name="money"/>
+      记账
+    </router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <Icon name="label"/>
+      标签
+    </router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
   export default {
-    name: 'Nav'
+    name: 'Nav',
   };
 </script>
 
 <style scoped lang="scss">
-  .nav{
+  .nav {
     border: 1px solid red;
   }
 </style>
