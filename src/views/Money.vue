80b5b81e1cca3339ae7845c9f75fdcc171b13b8e
shangjingfan <876625909@qq.com>
2021-03-09 18:48:20 +0800

    Label页面添加和展示标签

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,16 +14,18 @@
   import Notes from '@/components/Money/Notes.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component, Watch} from 'vue-property-decorator';
-  import model from '@/model';
+  import recordListModel from '@/models/recordListModel';
+  import tagListModel from '@/models/tagListModel';
 
   // fetch里已经声明了类型，recordList就不用再声明了
-  const recordList2 = model.fetch();
+  const recordList2 = recordListModel.fetch();
+  const tagList = tagListModel.fetch();
 
   @Component({
     components: {Tags, Notes, Types, NumberPad}
   })
   export default class Money extends Vue {
-    tags = ['衣', '食', '住', '行'];
+    tags = tagList;
 
     recordList: RecordItem[] = recordList2;
     record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
@@ -37,7 +39,7 @@
     }
 
     saveRecord() {
-      const record2: RecordItem = model.clone(this.record);
+      const record2: RecordItem = recordListModel.clone(this.record);
       record2.createAt = new Date();
       this.recordList.push(record2);
       console.log(this.recordList);
@@ -45,7 +47,7 @@
 
     @Watch('recordList')
     onRecordListChange() {
-      model.save(this.recordList);
+      recordListModel.save(this.recordList);
     }
   }
 </script>
