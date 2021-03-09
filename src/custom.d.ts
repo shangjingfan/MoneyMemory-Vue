7bb1187d5e528cf6ea6ea527a031c41cda3b97cd
shangjingfan <876625909@qq.com>
2021-03-10 01:24:05 +0800

    封装tagList的增删改查

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -20,5 +20,9 @@ type TagListModel = {
 
 interface Window {
   tagList: Tag[];
+  findTag: (id: string) => Tag | undefined;
   createTag: (name: string) => void;
+  removeTag: (id: string) => boolean;
+  updateTag: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
+  // updateTag: TagListModel['update']; // 也可以这么写
 }
