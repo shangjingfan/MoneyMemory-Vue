e110b20c2388c04dcfe988445fa94c3882767629
shangjingfan <876625909@qq.com>
2021-03-11 08:45:25 +0800

    Tags.vue使用Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -21,14 +21,15 @@
     computed: {
       tagList() {
         // TODO
-        // return this.$store.fetchTags();
-        return []
+        return this.$store.state.tagList;
       }
     }
   })
   export default class Tags extends Vue {
     selectedTags: string[] = [];
-
+    created(){
+      this.$store.commit('fetchTags')
+    }
     toggle(tag: string) {
       const index = this.selectedTags.indexOf(tag);
       if (index >= 0) {
@@ -44,9 +45,7 @@
       if (!name) {
         return window.alert('标签名不能为空'); // alert返回undefined
       }
-      // TODO
-      // store.createTag(name);
-
+      this.$store.commit('createTag', name);
     }
   }
 </script>
